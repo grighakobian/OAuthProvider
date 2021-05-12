60051c9cb1e7e106b2fad797e4a39ce89fa8e8bd
Grigor Hakobyan <grighakobian@gmail.com>
2021-05-12 14:26:06 +0400

    Add access token store

---
+++ OAuthProvider/Classes/AccessTokenStore.swift
@@ -0,0 +1,30 @@
+//    Copyright (c) 2021 Grigor Hakobyan <grighakobian@gmail.com>
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+
+
+public protocol AccessTokenStore {
+    /// Retrive current access token
+    func getAccessToken()->AccessTokenType?
+    /// Save access token
+    /// - Parameter accessToken: Access token to save
+    func saveAccessToken(_ accessToken: AccessTokenType) throws
+    /// Reset current access token
+    func resetAccessToken()
+}
